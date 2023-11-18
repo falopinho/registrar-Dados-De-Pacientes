d6319b979e058e5368eb1c63aa45bfac18649889
Fabinho <116457022+falopinho@users.noreply.github.com>
2023-11-18 16:58:30 -0300

    Update cadastro_loop.py

--- registrar DadosDe Pacientes/cadastro_loop.py
+++ registrar DadosDe Pacientes/cadastro_loop.py
@@ -1,3 +1,5 @@
+import cadastro
+
 #Loop para continuar cadastrando clientes
 def cadastro_loop():                                            
     app = input("Deseja cadastrar um cliente? ").lower()
@@ -5,9 +7,16 @@ def cadastro_loop():
     while True:
 
         if app in ["sim", "s", "si"]:
-            pass
+            cadastro.nome()
+            cadastro.email()
+            cadastro.cpf()
+            cadastro.rg()
+            cadastro.tel()
+            cadastro.nascimento()
         elif app in ["não", "nao", "n", "na"]:
             break
         else:
             print("Essa não é uma opção válida, por favor tente novamente...\n")
-            cadastro_loop()
+            app = input("Deseja cadastrar um cliente? ").lower()
+
+cadastro_loop()
