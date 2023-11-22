10753178c8372c29fc2fe79fc62146502fb538b6
Fabinho <116457022+falopinho@users.noreply.github.com>
2023-11-22 16:57:20 -0300

    Update main.py

--- registrar DadosDe Pacientes/main.py
+++ registrar DadosDe Pacientes/main.py
@@ -1,5 +1,22 @@
-import os
-import cadastro
-import cadastro_loop
+#Lógica por trás do código
 
-print(cadastro_loop.cadastro_loop)
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+
+cliente = nome,  idade
+cliente2 = nome, idade
+
+    
+if idade < 65:
+    with open(str(nome) + ".txt", "w") as arquivo:
+            for valor in cliente:
+                arquivo.write(str(valor) + "\n")
+
+    print("Cliente adicionado com sucesso")
+
+elif idade >= 65:
+    with open(str(nome) + ".txt", "w") as arquivo:
+        for valor in cliente2:
+            arquivo.write(str(valor) + "\n")
+else:
+    print("OPÇÃO INVÁLIDA!")
