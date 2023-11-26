780835ceb3454e08e2e9101764a9c6f8dd791b20
Fabinho <116457022+falopinho@users.noreply.github.com>
2023-11-26 20:30:27 -0300

    Update interface.py

--- registrar DadosDe Pacientes/interface.py
+++ registrar DadosDe Pacientes/interface.py
@@ -3,29 +3,30 @@ import buttom
 
 menu = ctk.CTk()
 menu.title("Cadastro de Pacientes")
-menu.geometry('500x300')
+menu.geometry('800x500')
+menu.resizable(width="False", height="False")
 
 
 #Configurando janela menu
 texto = ctk.CTkLabel(menu, text="Cadastro de Pacientes")
-texto.pack(padx="10", pady="20")
+texto.pack(padx="10", pady="40")
 
 texto1 = ctk.CTkLabel(menu, text="Deseja cadastrar um cliente?")
-texto1.pack(padx="10", pady="10")
+texto1.pack(padx="10", pady="50")
 
-#configurando janela main
-def main ():
-    main = ctk.CTkToplevel(menu)
-    main.geometry('500x300')
-    main.title("Cadastro de Pacientes")
-    
+#Nova Janela
+def main():
+    main = ctk.CTkToplevel(menu, fg_color="white")
+    main.geometry('800x500')
+    main.resizable(width="False", height="False")
+    main.mainloop()
 
 #botões sim e não
 botao = ctk.CTkButton(master=menu, text="Sim", command=main)
-botao.pack(padx="10", pady="20")
+botao.pack(padx="10", pady="30")
 
 botao1 = ctk.CTkButton(menu, text="Não", command=buttom.desligar)
-botao1.pack(padx="10", pady="20")
+botao1.pack(padx="10", pady="30")
 
 
 menu.mainloop()
